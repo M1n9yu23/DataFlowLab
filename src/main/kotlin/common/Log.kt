c297ab5631d1f68d01a746768fd88a71808f2f81
BossMG-github <128052197+BossMG-github@users.noreply.github.com>
2025-12-14 21:45:24 +0900

    로깅에 사용할 함수 구현

---
+++ src/main/kotlin/common/Log.kt
@@ -0,0 +1,23 @@
+package common
+
+// 필요할 때 모든 로그를 끌 때 사용
+object AppLogger {
+    var isDebugMode = true
+}
+
+fun Any.printLog(message: String) {
+    if (!AppLogger.isDebugMode) return
+
+    val tag = this::class.simpleName ?: "익명"
+
+    println("[$tag] $message")
+}
+
+// 타입이 없는 공간에서 사용할 때 사용
+inline fun <reified T> printLog(message: String) {
+    if (!AppLogger.isDebugMode) return
+
+    val tag = T::class.simpleName ?: "정적"
+
+    println("[$tag] $message")
+}
